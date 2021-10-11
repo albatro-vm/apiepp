bf6f9d6392d9afd660d7aead993fcb91744e09ca
Lilu <liamoreno14@gmail.com>
2021-10-11 19:01:40 -0300

    actualizo requerimientos

--- app-api.py
+++ app-api.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58a3761ffbb6507a0a92fe7fc438a67ac71618db4d46f542c244e64d1c3b10d5
-size 4458
+oid sha256:b4d132096e0d079799f6801b7b91f7a6bcd639a6755afdddbde0361a8320ab5b
+size 4461
